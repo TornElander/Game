423dda0ae3b5d577b77c8481bec295fbb688aaa4
Elena <marzi@student.chalmers.se>
2018-01-28 22:13:45 +0100

    First Commit

---
+++ HorrorGame/src/horrorgame/HorrorGame.java
@@ -0,0 +1,18 @@
+package horrorgame;
+
+/*
+ *  In the cimitery, when it is dark, strange things happen..
+ *  Enjoy your trip in to the abyss..
+ *
+ * @author Flisp
+ */
+public class HorrorGame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
